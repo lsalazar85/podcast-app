c994dfff9083c0cfd24b5bfdbb68572a2b4c2b83
Luis Salazar <l_salazar85@amco.mx>
2019-06-27 19:46:34 -0300

    Style jsx, About Component

---
+++ pages/about.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="main">
+            <h1>Luis Salazar</h1>
+            <h2>Frontend Developer</h2>
+            <div className="main-icons">
+                <img src="/static/platzi-logo.png" alt="Platzi" />
+                <img src="/static/react.png" alt="React" />
+                <img src="/static/next-logo.png" alt="Next" />
+            </div>
+            <p>
+                Practica React.js con Next.js
+            </p>
+
+            <style jsx>{`
+
+                .main {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    color: #fff;
+                }
+
+                h1 {
+                    color: #daedfd;
+                }
+
+                p {
+                    color
+                }
+
+                .main-icons {
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    margin: 20px 0 20px 0;
+                }
+
+                .main-icons img {
+                    width: 100px;
+                    height: auto;
+                    margin-right: 10px;
+                }
+
+                .main-icons img:last-child {
+                    margin-right: 0px;
+                }
+
+                
+            `}</style>
+
+            <style jsx global>{`
+                body {
+                    background-color: #323b4c;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,36 @@ import React from "react";
 
 const Home  = () => {
     return (
-        <h1>Hola Mundo!!!</h1>
+        <div className="main">
+            <h1>Hola Mundo <span>!!!</span></h1>
+            <div>
+                <img src="/static/platzi-logo.png" alt="Platzi" />
+            </div>
+
+            <style jsx>{`
+
+                .main {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+
+                h1 {
+                    color: red;
+                }
+
+                img {
+                    width: 100px;
+                    height: auto;
+                }
+            `}</style>
+
+            <style jsx global>{`
+                body {
+                    background-color: #fff;
+                }
+            `}</style>
+        </div>
     )
 }
 
